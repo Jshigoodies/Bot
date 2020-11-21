1c5f4d8b066c77cbcede9ed8b1005680413d3b60
unknown <jshigoodies@gmail.com>
2020-11-20 19:21:24 -0800

    commit

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import youtube_dl
 
 from discord.ext import commands
 
-token = "NzcyMTYwMTg2ODQ4MjQ3ODQ4.X52oNg.m5VMcixgM1EnjlzWL3t-LEYACF4"
+token = ""
 
 ID = 692572871322632222
 messaged = joined = 0
